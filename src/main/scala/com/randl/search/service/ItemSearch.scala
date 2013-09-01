ed2ee8155738688e7026ee6055f3e08cd9923119
cgonzalezg <cesar.gonzalez@holidaycheck.com>
2013-09-01 18:44:23 +0200

    first version needs to refactor

--- src/main/scala/com/randl/search/service/ItemSearch.scala
+++ src/main/scala/com/randl/search/service/ItemSearch.scala
@@ -4,6 +4,7 @@ import com.randl.search.service.resquest.RentItem
 import org.elasticsearch.index.query.QueryBuilders
 import java.util.UUID
 import org.elasticsearch.common.geo.GeoPoint
+import java.lang.{Double => jDouble}
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,25 +13,31 @@ import org.elasticsearch.common.geo.GeoPoint
  * Time: 11:44 AM
  * To change this template use File | Settings | File Templates.
  */
-trait ItemSearch extends ESClient{
+trait ItemSearch extends ESClient {
 
 
   import scala.collection.JavaConversions._
 
   def getItemES(id: String): RentItem = {
     val search = client.prepareSearch("rendl").setTypes("item")
-      .setQuery(QueryBuilders.fieldQuery("id",id)).execute().actionGet()
+      .setQuery(QueryBuilders.fieldQuery("id", id)).execute().actionGet()
     search.getHits.getHits.toList match {
-      case head :: tail => RentItem(
-        id = UUID.fromString(head.id()),
-        description = head.getSource.get("description").asInstanceOf[String],
-        location = head.getSource.get("location").asInstanceOf[GeoPoint],
-        name = head.getSource.get("name").asInstanceOf[String],
-        picture = head.getSource.get("picture").asInstanceOf[java.util.List[String]].toList,
-        price = head.getSource.get("price").asInstanceOf[Int],
-        category = head.getSource.get("picture").asInstanceOf[java.util.List[String]].toList,
-        user = UUID.fromString(head.getSource.get("user").asInstanceOf[String])
-      )
+
+      case head :: tail =>
+        val price: jDouble = head.getSource.get("price") match {
+          case y: java.lang.Integer => new jDouble(y * 1.0)
+          case _ => head.getSource.get("price").asInstanceOf[jDouble]
+        }
+        RentItem(
+          id = UUID.fromString(head.id()),
+          description = head.getSource.get("description").asInstanceOf[String],
+          location = head.getSource.get("location").asInstanceOf[GeoPoint],
+          name = head.getSource.get("name").asInstanceOf[String],
+          picture = head.getSource.get("picture").asInstanceOf[java.util.List[String]].toList,
+          price = price,
+          category = head.getSource.get("picture").asInstanceOf[java.util.List[String]].toList,
+          user = UUID.fromString(head.getSource.get("user").asInstanceOf[String])
+        )
       case _ => null
     }
 
